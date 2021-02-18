75a83aa09a5c93b10f01e6539c17ec87d3597dc5
Pepe Valdivia <jvaldivia@softweb.pe>
2021-02-18 16:00:47 -0500

    editar un pdf

--- app.js
+++ app.js
@@ -1,13 +1,69 @@
-import { PDFDocument } from 'pdf-lib'
+import { PDFDocument, StandardFonts, degrees } from 'pdf-lib'
 import fs from 'fs'
 
-// PDF Creation
-async function start() {
+function randomSN(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+async function create() {
   const pdfDoc = await PDFDocument.create()
   const page = pdfDoc.addPage()
   page.drawText('You can create PDFs!')
   const pdfBytes = await pdfDoc.save()
-  fs.writeFileSync('./tmp/test.pdf', pdfBytes);
+  fs.writeFileSync(`./tmp/${randomSN(5)}.pdf`, pdfBytes);
 }
 
-start().catch(err => console.log(err));
+// create().catch(err => console.log(err));
+
+async function edit(name = 'Carlo Tevez') {
+  const url = './tmp/Empty.pdf'
+  const existingPdfBytes = await fs.readFileSync(url);
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+  const { width, height } = firstPage.getSize()
+  firstPage.drawText(name, {
+    x: 300,
+    y: 318,
+    size: 19,
+    font: helveticaFont,
+    //color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(90),
+  })
+
+  const pdfBytes = await pdfDoc.save();
+  fs.writeFileSync(`./tmp/${randomSN(5)}.pdf`, pdfBytes);
+}
+
+var alumnos = [
+  {
+    nombre: 'Pepe Valdivia',
+    correo: 'pepe.valdivia.caballero@gmail.com',
+    nota: 14,
+  },
+  {
+    nombre: 'Yacky Ramiréz',
+    correo: 'pepe.valdivia.caballero@gmail.com',
+    nota: 17,
+  },
+  {
+    nombre: 'Sila Esculapia',
+    correo: 'pepe.valdivia.caballero@gmail.com',
+    nota: 20,
+  },
+]
+
+alumnos.forEach(alumno => {
+  edit(alumno.nombre).catch(err => console.log(err));
+});
+
+// edit('Yacky Ramirez Rodrigues').catch(err => console.log(err));

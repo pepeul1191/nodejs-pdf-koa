c84591cb60f0408a735222416727a10c72f36f7d
Pepe Valdivia <jvaldivia@softweb.pe>
2021-02-19 21:57:48 -0500

    bower

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
   data: {
     sistema_id: 1,
-    base_url: 'http://localhost:3001/',
-    static_url: 'http://localhost:3001/',
+    base_url: 'http://localhost:3000/',
+    static_url: 'http://localhost:3000/',
     static: 'dev',
     csrf: {
       secret: 'mpt/sr6eS2AlCRHU7DVThMgFTN08pnfSDf/C94eZx7udfm0lvgaYWLYJttYPKzGKDTlXwVU/d2FOxbKkgNlsTw==',
